3b0028b25ac69a366d883f45d98dca5db7d23975
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2025-03-04 18:00:36 +0500

    set up color palete for screens

---
+++ lib/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color myPrimaryColor = Color(0xFF51A49A);  // Custom teal color
+const Color bgColor = Color(0xFFFFFFFF); // Example secondary color
---
+++ lib/views/splash/spalsh_1.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Spalsh1 extends StatefulWidget {
+  const Spalsh1({super.key});
+
+  @override
+  State<Spalsh1> createState() => _Spalsh1State();
+}
+
+class _Spalsh1State extends State<Spalsh1> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+
+      body: Center(child: Text('Splash Screen'),),
+
+    );
+
+  }
+}

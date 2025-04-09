1d6b4d2b5be403965afe0d01f41be685bd502ed6
Zulfiqar Alam <zulfi18.official@gmail.com>
2025-04-09 13:41:21 +0500

    color palet for whole app

---
+++ lib/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Colors.teal;
+  static const Color secondary = Colors.white;
+  static const Color myRedColor = Colors.red;
+  static const Color background = Color(0xFFF0F0F0);
+  static const Color myColor = Color.fromRGBO(217, 217, 217, 0.5);
+
+}
+
